424af6f8a9da726cf6ceea62834bd6dd667722cf
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-11-08 23:11:26 +0100

    fix(Housings.model): return null if no matching housing found

--- src/mocked/Housings.model.js
+++ src/mocked/Housings.model.js
@@ -14,5 +14,6 @@ export async function getHousings() {
  * @returns {Promise<*>}
  */
 export async function getHousing(id) {
-    return housings.find((housing) => housing.id === id);
+    const housing = housings.find((housing) => housing.id === id);
+    return housing ?? null;
 }
