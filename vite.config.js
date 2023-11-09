fc9232016df7b78198e3cba2387594d34ec676e6
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-11-09 01:19:37 +0100

    feat: add global scss variables import in Vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "./src/styles/variables.scss" as *;`,
+      },
+    },
+  },
   plugins: [react()],
 })
