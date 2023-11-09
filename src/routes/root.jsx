a1643e435b4996068c5d32fcc155c9186addb134
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-11-09 01:20:16 +0100

    feat: add root style

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import styles from './root.module.scss';
 
 export default function Root() {
   return (
     <>
-      <Header />
-      <Outlet />
+      <div className={styles.pageWrapper}>
+        <Header />
+        <Outlet />
+      </div>
       <Footer />
     </>
   );
