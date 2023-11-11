c3ca644069fd291f41cab79bd923b1c3a2e58505
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-11-11 02:59:40 +0100

    feat: complete index page with responsive style

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -1,10 +1,11 @@
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import type { LoaderFunctionArgs } from 'react-router-dom';
 import type { LoaderData } from '../types/react-router';
 import type { Housing } from '../types/kasa.ts';
 import heroImage from '../assets/hero1.png';
 import Hero from '../components/Hero';
 import HousingCard from '../components/HousingCard';
+import styles from './_index.module.scss';
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const res = await fetch(`/logements.json`, {
@@ -16,17 +17,23 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function HomePage() {
   const { housings } = useLoaderData() as LoaderData<typeof loader>;
-  console.log('Home: ', housings);
+
   return (
-    <div>
+    <>
       <Hero
         imageUrl={heroImage}
         imageAlt="Paysage saisissant de falaises le long de la mer."
       >
-        Chez vous, partout et ailleurs
+        Chez vous,
+        <br className={styles.mobileOnly} aria-hidden /> partout et ailleurs
       </Hero>
-      <HousingCard housing={housings[0]} />
-      <pre>{JSON.stringify(housings, null, 2)}</pre>
-    </div>
+      <div className={styles.housingsContainer}>
+        {housings.map((housing) => (
+          <Link to={`/housings/${housing.id}`} key={housing.id}>
+            <HousingCard rootClassName={styles.housingCard} housing={housing} />
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
