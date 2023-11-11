fa08e31dc6a723a240c3ddaf5271abf5f435d40b
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-11-11 02:58:24 +0100

    feat: allow parent component to pass css class to HousingCard to control its size (and more)

--- src/components/HousingCard.tsx
+++ src/components/HousingCard.tsx
@@ -3,11 +3,12 @@ import type { Housing } from '../types/kasa.ts';
 
 interface HousingProps {
   housing: Housing;
+  rootClassName?: string;
 }
 
-export default function HousingCard({ housing }: HousingProps) {
+export default function HousingCard({ housing, rootClassName }: HousingProps) {
   return (
-    <div className={styles.card}>
+    <div className={`${styles.card} ${rootClassName}`}>
       <img
         src={housing.cover}
         alt={`Cover photo of ${housing.title}`}
