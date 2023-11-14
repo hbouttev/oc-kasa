2a0a435b965323c4755f81f991856bf82d9b441d
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-11-14 01:02:21 +0100

    feat: add rating stars component

---
+++ src/components/RatingStars.tsx
@@ -0,0 +1,31 @@
+import styles from './RatingStars.module.scss';
+import StarActive from '../assets/icons/star-active.svg?react';
+import StarInactive from '../assets/icons/star-inactive.svg?react';
+
+interface RatingStarsProps {
+  rating: number;
+}
+
+export default function RatingStars({ rating }: RatingStarsProps) {
+  const activeStars = Array.from({ length: rating }, (_, i) => i + 1);
+  let inactiveStars: number[] = [];
+  if (rating < 5) {
+    inactiveStars = Array.from(
+      { length: 5 - rating },
+      (_, i) => rating + 1 + i
+    );
+  }
+  console.log('rating: ', rating);
+  console.log('activeStars: ', activeStars);
+  console.log('inactiveStars: ', inactiveStars);
+  return (
+    <div className={styles.starsContainer}>
+      {activeStars.map((star) => (
+        <StarActive key={star} className={styles.star} />
+      ))}
+      {inactiveStars.map((star) => (
+        <StarInactive key={star} className={styles.star} />
+      ))}
+    </div>
+  );
+}
