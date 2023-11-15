ce8c26db673a4eba42319b1200fdcdaad2fc6455
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-11-16 00:40:57 +0100

    feat: complete housing page (missing responsive)

--- src/routes/housings.$housingId.tsx
+++ src/routes/housings.$housingId.tsx
@@ -2,6 +2,12 @@ import { useLoaderData } from 'react-router-dom';
 import type { LoaderFunctionArgs } from 'react-router-dom';
 import type { LoaderData } from '../types/react-router';
 import type { Housing } from '../types/kasa.ts';
+import styles from './housings.$housingId.module.scss';
+import ImageCarousel from '../components/ImageCarousel.tsx';
+import RatingStars from '../components/RatingStars.tsx';
+import Tag from '../components/Tag.tsx';
+import HostCard from '../components/HostCard.tsx';
+import Collapsible from '../components/Collapsible.tsx';
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
   const res = await fetch(`/logements.json`, {
@@ -18,16 +24,38 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 export default function HousingPage() {
   const { housing } = useLoaderData() as LoaderData<typeof loader>;
   return (
-    <div>
-      <h2>Housing</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-        voluptatum, quibusdam, quia, quae voluptates voluptatem quod
-        voluptatibus quos doloribus quidem voluptate. Quisquam, quibusdam, quia,
-        quae voluptates voluptatem quod voluptatibus quos doloribus quidem
-        voluptate.
-      </p>
-      <pre>{JSON.stringify(housing, null, 2)}</pre>
-    </div>
+    <>
+      <div className={styles.carouselContainer}>
+        <ImageCarousel images={housing.pictures} />
+      </div>
+      <div className={styles.headerContainer}>
+        <div className={styles.headerOverview}>
+          <h1 className={styles.title}>{housing.title}</h1>
+          <p className={styles.location}>{housing.location}</p>
+          <div className={styles.tags}>
+            {housing.tags.map((tag) => (
+              <Tag key={tag} tag={tag} />
+            ))}
+          </div>
+        </div>
+        <div className={styles.headerInfos}>
+          <HostCard name={housing.host.name} picture={housing.host.picture} />
+          <RatingStars rating={parseInt(housing.rating)} />
+        </div>
+      </div>
+
+      <div className={styles.detailsContainer}>
+        <Collapsible title="Description" titleTagName="h2" size="medium">
+          <p>{housing.description}</p>
+        </Collapsible>
+        <Collapsible title="Équipements" titleTagName="h2" size="medium">
+          <ul>
+            {housing.equipments.map((equipment) => (
+              <li key={equipment}>{equipment}</li>
+            ))}
+          </ul>
+        </Collapsible>
+      </div>
+    </>
   );
 }
